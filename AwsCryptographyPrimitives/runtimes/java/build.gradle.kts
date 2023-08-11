df9d7b641c813cfbe99d8da8ca1f822675bcfca8
texastony <5892063+texastony@users.noreply.github.com>
2023-08-11 15:12:21 -0600

    WIP: ACCP HKDF

--- AwsCryptographyPrimitives/runtimes/java/build.gradle.kts
+++ AwsCryptographyPrimitives/runtimes/java/build.gradle.kts
@@ -4,6 +4,8 @@
 plugins {
     `java-library`
     `maven-publish`
+    // OS Detector for Optional ACCP
+    id("com.google.osdetector") version "1.7.0"
 }
 
 group = "software.amazon.cryptography"
@@ -20,6 +22,17 @@ java {
     sourceSets["test"].java {
         srcDir("src/test/dafny-generated")
     }
+//    sourceSets.create("amazonCorrettoCryptoProvider").java {
+//        srcDir("src/main/java")
+//        srcDir("src/main/dafny-generated")
+//        srcDir("src/main/smithy-generated")
+//        srcDir("src/accp/java")
+//    }
+    // Optional ACCP dependency for at least HKDF
+    registerFeature("amazonCorrettoCryptoProvider") {
+        // We may create a new source set with ACCP code...
+        usingSourceSet(sourceSets["main"])
+    }
 }
 
 repositories {
@@ -31,7 +44,10 @@ dependencies {
     implementation("org.dafny:DafnyRuntime:4.1.0")
     implementation("software.amazon.smithy.dafny:conversion:0.1")
     implementation("software.amazon.cryptography:StandardLibrary:1.0-SNAPSHOT")
-    implementation("org.bouncycastle:bcprov-jdk18on:1.72")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.75")
+    // "amazonCorrettoCryptoProviderImplementation"(
+    implementation(
+            "software.amazon.cryptools:AmazonCorrettoCryptoProvider:2.3.0:${determineClassifier()}")
 }
 
 publishing {
@@ -52,4 +68,15 @@ tasks {
         mainClass.set("TestsFromDafny")
         classpath = sourceSets["test"].runtimeClasspath
     }
+}
+
+// ACCP ONLY supports Linux and Mac
+// https://github.com/corretto/amazon-corretto-crypto-provider/tree/main#compatibility--requirements
+// But, they DO NOT release a Mac Version,
+// they just call it linux.
+fun determineClassifier(): String {
+    if (osdetector.os.contains("osx")) {
+        return "linux-" + osdetector.arch
+    }
+    return osdetector.classifier
 }
