4a6f9fd9070dac725c3626aba2f583420a9d60e5
texastony <5892063+texastony@users.noreply.github.com>
2023-08-15 15:18:47 -0700

    refactor(Java): re-enable publishToMavenLocal

--- AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -1,5 +1,9 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 import java.net.URI
-import javax.annotation.Nullable
+
+val publishedVersion: String by project
+val verboseTesting: Boolean by project
 
 plugins {
     `java-library`
@@ -10,7 +14,7 @@ plugins {
 }
 
 group = "software.amazon.cryptography"
-version = "1.0.1"
+version = if (project.hasProperty("publishedVersion")) publishedVersion else "1.0-SNAPSHOT"
 description = "AWS Cryptographic Material Providers Library"
 
 java {
@@ -25,33 +29,9 @@ java {
     withSourcesJar()
 }
 
-var caUrl: URI? = null
-@Nullable
-val caUrlStr: String? = System.getenv("CODEARTIFACT_URL_JAVA_CONVERSION")
-if (!caUrlStr.isNullOrBlank()) {
-    caUrl = URI.create(caUrlStr)
-}
-
-var caPassword: String? = null
-@Nullable
-val caPasswordString: String? = System.getenv("CODEARTIFACT_AUTH_TOKEN")
-if (!caPasswordString.isNullOrBlank()) {
-    caPassword = caPasswordString
-}
-
 repositories {
     mavenCentral()
     mavenLocal()
-    if (caUrl != null && caPassword != null) {
-        maven {
-            name = "CodeArtifact"
-            url = caUrl!!
-            credentials {
-                username = "aws"
-                password = caPassword!!
-            }
-        }
-    }
 }
 
 dependencies {
@@ -78,20 +58,6 @@ dependencies {
 }
 
 publishing {
-    publications.create<MavenPublication>("mavenLocal") {
-        groupId = "software.amazon.cryptography"
-        artifactId = "aws-cryptographic-material-providers"
-        artifact(tasks["shadowJar"])
-        artifact(tasks["javadocJar"])
-        artifact(tasks["sourcesJar"])
-
-        // Since we ship the MPL bundled with our generated dependencies they should not be included in the generated pom.xml
-        // however; we also use additional dependencies runtime dependencies that are needed in order to properly run the mpl.
-        // When you bundle a shadow jar you don't need to include any dependencies in the pom.xml since everything is on the jar, but since
-        // we are selective so we have to "manually" write our own pom file.
-        buildPom(this )
-    }
-
     publications.create<MavenPublication>("maven") {
         groupId = "software.amazon.cryptography"
         artifactId = "aws-cryptographic-material-providers"
@@ -108,12 +74,14 @@ publishing {
 
     repositories {
         mavenLocal()
-        maven {
-            name = "PublishToCodeArtifactStaging"
-            url = URI.create("https://crypto-tools-internal-587316601012.d.codeartifact.us-east-1.amazonaws.com/maven/java-mpl-staging/")
-            credentials {
-                username = "aws"
-                password = System.getenv("CODEARTIFACT_TOKEN")
+        if (project.hasProperty("publishedVersion")) {
+            maven {
+                name = "PublishToCodeArtifactStaging"
+                url = URI.create("https://crypto-tools-internal-587316601012.d.codeartifact.us-east-1.amazonaws.com/maven/java-mpl-staging/")
+                credentials {
+                    username = "aws"
+                    password = System.getenv("CODEARTIFACT_TOKEN")
+                }
             }
         }
     }
@@ -184,23 +152,23 @@ nexusPublishing {
     }
 }
 
-signing {
-    useGpgCmd()
+if (project.hasProperty("publishedVersion")) {
+    signing {
+        useGpgCmd()
 
-    // Dynamically set these properties
-    project.ext.set("signing.gnupg.executable", "gpg")
-    project.ext.set("signing.gnupg.useLegacyGpg" , "true")
-    project.ext.set("signing.gnupg.homeDir", System.getenv("HOME") + "/.gnupg/")
-    project.ext.set("signing.gnupg.optionsFile", System.getenv("HOME") + "/.gnupg/gpg.conf")
-    project.ext.set("signing.gnupg.keyName", System.getenv("GPG_KEY"))
-    project.ext.set("signing.gnupg.passphrase", System.getenv("GPG_PASS"))
+        // Dynamically set these properties
+        project.ext.set("signing.gnupg.executable", "gpg")
+        project.ext.set("signing.gnupg.useLegacyGpg", "true")
+        project.ext.set("signing.gnupg.homeDir", System.getenv("HOME") + "/.gnupg/")
+        project.ext.set("signing.gnupg.optionsFile", System.getenv("HOME") + "/.gnupg/gpg.conf")
+        project.ext.set("signing.gnupg.keyName", System.getenv("GPG_KEY"))
+        project.ext.set("signing.gnupg.passphrase", System.getenv("GPG_PASS"))
 
-    // Signing is required if building a release version and if we're going to publish it.
-    // Otherwise if doing a maven publication we will sign
-    setRequired({
-        gradle.getTaskGraph().hasTask("publish")
-    })
-    sign(publishing.publications["maven"])
+        // Signing is required if building a release version and if we're going to publish it.
+        // Otherwise if doing a maven publication we will sign
+        setRequired({ gradle.getTaskGraph().hasTask("publish") })
+        sign(publishing.publications["maven"])
+    }
 }
 
 fun SourceDirectorySet.mainSourceSet() {
@@ -216,9 +184,13 @@ tasks.test {
     // This will show System.out.println statements
     testLogging.showStandardStreams = true
 
+    var whatToLog = mutableSetOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED, org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED)
+    if (project.hasProperty("verboseTesting")) {
+        whatToLog = mutableSetOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED, org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED, org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED)
+    }
     testLogging {
         lifecycle {
-            events = mutableSetOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED, org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED, org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED)
+            events = whatToLog
             exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
             showExceptions = true
             showCauses = true
