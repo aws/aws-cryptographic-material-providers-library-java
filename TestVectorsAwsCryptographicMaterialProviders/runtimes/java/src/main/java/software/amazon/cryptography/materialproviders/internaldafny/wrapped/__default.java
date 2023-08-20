f303b71c62468b6889e1dbbdb4e9c067f9fa7aff
texastony <5892063+texastony@users.noreply.github.com>
2023-08-19 18:03:26 -0700

    test: Log HKDF Provider

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/java/src/main/java/software/amazon/cryptography/materialproviders/internaldafny/wrapped/__default.java
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/src/main/java/software/amazon/cryptography/materialproviders/internaldafny/wrapped/__default.java
@@ -1,18 +1,27 @@
 package software.amazon.cryptography.materialproviders.internaldafny.wrapped;
 
+import java.util.logging.Logger;
+
 import software.amazon.cryptography.materialproviders.internaldafny.types.Error;
 import software.amazon.cryptography.materialproviders.internaldafny.types.IAwsCryptographicMaterialProvidersClient;
 import software.amazon.cryptography.materialproviders.internaldafny.types.MaterialProvidersConfig;
+
 import Wrappers_Compile.Result;
+
+import software.amazon.cryptography.primitives.model.GetHKDFProviderInput;
+import software.amazon.cryptography.primitives.model.HKDFProvider;
 import software.amazon.cryptography.materialproviders.MaterialProviders;
 import software.amazon.cryptography.materialproviders.ToNative;
 import software.amazon.cryptography.materialproviders.wrapped.TestMaterialProviders;
 
 public class __default extends _ExternBase___default {
-    public static Result<IAwsCryptographicMaterialProvidersClient, Error> WrappedMaterialProviders(MaterialProvidersConfig config) {
-        software.amazon.cryptography.materialproviders.model.MaterialProvidersConfig wrappedConfig = ToNative.MaterialProvidersConfig(config);
-        software.amazon.cryptography.materialproviders.MaterialProviders impl = MaterialProviders.builder().MaterialProvidersConfig(wrappedConfig).build();
-        TestMaterialProviders wrappedClient = TestMaterialProviders.builder().impl(impl).build();
-        return Result.create_Success(wrappedClient);
-    }
+  private static final Logger LOGGER = Logger.getLogger(__default.class.getName());
+  public static Result<IAwsCryptographicMaterialProvidersClient, Error> WrappedMaterialProviders(MaterialProvidersConfig config) {
+    software.amazon.cryptography.materialproviders.model.MaterialProvidersConfig wrappedConfig = ToNative.MaterialProvidersConfig(config);
+    software.amazon.cryptography.materialproviders.MaterialProviders impl = MaterialProviders.builder().MaterialProvidersConfig(wrappedConfig).build();
+    HKDFProvider hkdfProvider = impl.GetHKDFProvider(GetHKDFProviderInput.builder().build()).provider();
+    LOGGER.warning(String.format("HKDF Provider is %s", hkdfProvider));
+    TestMaterialProviders wrappedClient = TestMaterialProviders.builder().impl(impl).build();
+    return Result.create_Success(wrappedClient);
+  }
 }
